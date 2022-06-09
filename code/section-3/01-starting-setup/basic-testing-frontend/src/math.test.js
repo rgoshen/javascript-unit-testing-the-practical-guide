72c6a1153cf157453199aa48c0fac73a95d88db5
Richard Goshen <rick.goshen@gmail.com>
2022-06-08 21:49:08 -0700

    add first test for math function

---
+++ code/section-3/01-starting-setup/basic-testing-frontend/src/math.test.js
@@ -0,0 +1,7 @@
+import { expect, it } from "vitest";
+import { add } from "./math";
+
+it("should sum up all numbers in an array", () => {
+  const result = add([1, 2, 3]);
+  expect(result).toEqual(6);
+});
