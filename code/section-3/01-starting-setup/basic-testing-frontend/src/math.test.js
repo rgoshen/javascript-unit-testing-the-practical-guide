7b4855c759d239c784ef74c504b6953d2ce4d806
Richard Goshen <rick.goshen@gmail.com>
2022-06-09 21:30:57 -0700

    Update math.test.js to add two additional tests
    Added test for ensure add function returns a NaN if invalid input and also test to return correct sum even if numeric string values are passed in.

--- code/section-3/01-starting-setup/basic-testing-frontend/src/math.test.js
+++ code/section-3/01-starting-setup/basic-testing-frontend/src/math.test.js
@@ -2,6 +2,39 @@ import { expect, it } from 'vitest';
 import { add } from './math';
 
 it('should sum up all numbers in an array', () => {
-  const result = add([1, 2, 3]);
-  expect(result).toEqual(6);
+  // Arrange
+  const numbers = [1, 2, 3, 4, 5];
+  const expectedResult = numbers.reduce((sum, number) => sum + number, 0);
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  expect(result).toEqual(expectedResult);
+});
+
+it('should yield NaN if at least one invalid number is provided', () => {
+  // Arrange
+  const numbers = [1, 2, 'invalid', 4, 5];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  expect(result).toBeNaN();
+});
+
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+  // Arrange
+  const numbers = ['1', '2', '3', '4', '5'];
+  const expectedResult = numbers.reduce(
+    (sum, number) => sum + parseInt(number, 10),
+    0
+  );
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  expect(result).toEqual(expectedResult);
 });
