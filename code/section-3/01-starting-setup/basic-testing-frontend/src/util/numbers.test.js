d0c4b025f09b03d31b82921b2c037cc2d9049922
Richard Goshen <rick.goshen@gmail.com>
2022-06-10 13:52:06 -0700

    add describe block

--- code/section-3/01-starting-setup/basic-testing-frontend/src/util/numbers.test.js
+++ code/section-3/01-starting-setup/basic-testing-frontend/src/util/numbers.test.js
@@ -1,42 +1,44 @@
-import { it, expect } from 'vitest';
+import { it, expect, describe } from 'vitest';
 import { transformToNumber } from './numbers';
 
-it('should transform a string  number to a number of type number', () => {
-  const input = '1';
-  const result = transformToNumber(input);
+describe('transformToNumber', () => {
+  it('should transform a string  number to a number of type number', () => {
+    const input = '1';
+    const result = transformToNumber(input);
 
-  expect(result).toEqual(+input);
-  expect(result).toBeTypeOf('number');
-});
+    expect(result).toEqual(+input);
+    expect(result).toBeTypeOf('number');
+  });
 
-it('should return NaN when a nontransformable value is passed in', () => {
-  const input = 'a1';
-  const input2 = {};
+  it('should return NaN when a nontransformable value is passed in', () => {
+    const input = 'a1';
+    const input2 = {};
 
-  const result = transformToNumber(input);
-  const result2 = transformToNumber(input2);
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
 
-  expect(result).toBeNaN();
-  expect(result2).toBeNaN();
-});
+    expect(result).toBeNaN();
+    expect(result2).toBeNaN();
+  });
 
-it('should return that number of type number when a number is passed in', () => {
-  const input = 1;
-  const result = transformToNumber(input);
+  it('should return that number of type number when a number is passed in', () => {
+    const input = 1;
+    const result = transformToNumber(input);
 
-  expect(result).toEqual(input);
-  expect(result).toBeTypeOf('number');
-});
+    expect(result).toEqual(input);
+    expect(result).toBeTypeOf('number');
+  });
 
-it('should return NaN when nothing is passed in', () => {
-  const result = transformToNumber();
+  it('should return NaN when nothing is passed in', () => {
+    const result = transformToNumber();
 
-  expect(result).toBeNaN();
-});
+    expect(result).toBeNaN();
+  });
 
-it('should return 0 when an empty string is passed in', () => {
-  const input = '';
-  const result = transformToNumber(input);
+  it('should return 0 when an empty string is passed in', () => {
+    const input = '';
+    const result = transformToNumber(input);
 
-  expect(result).toEqual(0);
+    expect(result).toEqual(0);
+  });
 });
