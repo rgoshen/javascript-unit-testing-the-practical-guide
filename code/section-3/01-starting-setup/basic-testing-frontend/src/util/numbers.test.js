12ecb43e96f5f1fef6b607b58b3681fc5248c2f8
Richard Goshen <rick.goshen@gmail.com>
2022-06-10 13:04:18 -0700

    Fix failing tests

--- code/section-3/01-starting-setup/basic-testing-frontend/src/util/numbers.test.js
+++ code/section-3/01-starting-setup/basic-testing-frontend/src/util/numbers.test.js
@@ -1,34 +1,38 @@
-import { it, expect } from "vitest";
-import { transformToNumber } from "./numbers";
+import { it, expect } from 'vitest';
+import { transformToNumber } from './numbers';
 
-it("should transform a string  number to a number of type number", () => {
-  const result = transformToNumber("1");
+it('should transform a string  number to a number of type number', () => {
+  const input = '1';
+  const result = transformToNumber(input);
 
-  expect(result).toEqual(1);
-  expect(result).toBeInstanceOf(Number);
+  expect(result).toEqual(+input);
+  expect(result).toBeTypeOf('number');
 });
 
-it("should return NaN when a non-numeric string is passed in", () => {
-  const result = transformToNumber("a1");
+it('should return NaN when a non-numeric string is passed in', () => {
+  const input = 'a1';
+  const result = transformToNumber(input);
 
   expect(result).toBeNaN();
 });
 
-it("should return that number of type number when a number is passed in", () => {
-  const result = transformToNumber(1);
+it('should return that number of type number when a number is passed in', () => {
+  const input = 1;
+  const result = transformToNumber(input);
 
-  expect(result).toEqual(1);
-  expect(result).toBeInstanceOf(Number);
+  expect(result).toEqual(input);
+  expect(result).toBeTypeOf('number');
 });
 
-it("should return NaN when nothing is passed in", () => {
+it('should return NaN when nothing is passed in', () => {
   const result = transformToNumber();
 
   expect(result).toBeNaN();
 });
 
-it("should return 0 when an empty string is passed in", () => {
-  const result = transformToNumber(" ");
+it('should return 0 when an empty string is passed in', () => {
+  const input = '';
+  const result = transformToNumber(input);
 
   expect(result).toEqual(0);
 });
