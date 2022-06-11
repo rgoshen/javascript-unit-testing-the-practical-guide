8a0205f39d9c05e001d5586edd0e99afd03bcd80
rgoshen <rick.goshen@gmail.com>
2022-06-10 22:27:31 -0700

    WIP - issue with importing numbers.js

--- code/section-4/01-starting-setup/src/math.js
+++ code/section-4/01-starting-setup/src/math.js
@@ -1,10 +1,24 @@
+import { cleanNumbers } from './util/numbers';
+
 export function add(numbers) {
   let sum = 0;
-
-  // throw new Error('Something went wrong');
 
   for (const number of numbers) {
     sum += +number;
   }
   return sum;
+}
+
+export function calculateResult(numberValues) {
+  let result = '';
+  try {
+    const numbers = cleanNumbers(numberValues);
+
+    console.log('numbers:::::', numbers);
+
+    result = add(numbers).toString();
+  } catch (error) {
+    result = error.message;
+  }
+  return result;
 }
