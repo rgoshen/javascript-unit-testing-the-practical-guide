9c29cee1b0de42802166a2330b41da7076c6784a
Richard Goshen <rick.goshen@gmail.com>
2022-06-12 14:32:30 -0700

    Update io.test.js
    add mock to remove writing text.txt side effect

--- code/section-7/01-starting-project/src/util/io.test.js
+++ code/section-7/01-starting-project/src/util/io.test.js
@@ -1,13 +1,18 @@
-import { it, describe, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import writeData from './io';
-
+import { promises as fs } from 'fs';
 describe('writeData()', () => {
+  vi.mock('fs'); // kicks in vi automocks for fs...vi will go find fs and resplace with a series of spies
   it('should execute the writeFile method', () => {
     const testData = 'test data';
     const testFilename = 'test.txt';
 
+    writeData(testData, testFilename);
+
     // Side effect: writes test data to test.txt and stores in data folder
     // this could be bad because it could overwrite or delete prod data
     // return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+
+    expect(fs.writeFile).toBeCalled();
   });
 });
