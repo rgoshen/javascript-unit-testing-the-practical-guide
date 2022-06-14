0cd24e77c00e286f303e8de9a3ea05c656512c16
rgoshen <rick.goshen@gmail.com>
2022-06-13 17:27:35 -0700

    Refactored writeFile test
    added mock of path.join to return filename to simplify the test

--- code/section-7/01-starting-project/src/util/io.test.js
+++ code/section-7/01-starting-project/src/util/io.test.js
@@ -2,7 +2,18 @@ import { describe, it, expect, vi } from 'vitest';
 import writeData from './io';
 import { promises as fs } from 'fs';
 describe('writeData()', () => {
-  vi.mock('fs'); // kicks in vi automocks for fs...vi will go find fs and resplace with a series of spies
+  vi.mock('fs'); // kicks in vi automocks for fs...vi will go find fs and replace with a series of spies
+
+  // mock out the path.join function because we just want it to return the filename so it will be easier to test
+  vi.mock('path', () => {
+    return {
+      default: {
+        join: (...args) => {
+          return args[args.length - 1]; // last argument is the filename
+        },
+      },
+    };
+  }); //we don't want vi to automock this, so we'll just mock it out by passing in a function as a second argument
   it('should execute the writeFile method', () => {
     const testData = 'test data';
     const testFilename = 'test.txt';
@@ -13,6 +24,8 @@ describe('writeData()', () => {
     // this could be bad because it could overwrite or delete prod data
     // return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
 
-    expect(fs.writeFile).toBeCalled();
+    // expect(fs.writeFile).toBeCalled();
+
+    expect(fs.writeFile).toBeCalledWith(testFilename, testData);
   });
 });
