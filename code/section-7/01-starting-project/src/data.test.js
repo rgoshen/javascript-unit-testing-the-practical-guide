a0bcd19fabd7ec8352ed10cb1bfd8d802d558292
rgoshen <rick.goshen@gmail.com>
2022-06-13 17:52:13 -0700

    add logger mockImplementation

--- code/section-7/01-starting-project/src/data.test.js
+++ code/section-7/01-starting-project/src/data.test.js
@@ -5,6 +5,9 @@ describe('generateReportData() ', () => {
   it('should exeute logFn if provided', () => {
     const logger = vi.fn(); //spy function
 
+    logger.mockImplementation(() => {}); //mock function that will replace the logger mock from line 6 above
+    // if used with mockImplementationOnce(), it will only be called once and then go back to the original implementation from line 6 above
+
     generateReportData(logger);
 
     expect(logger).toHaveBeenCalled();
